c80cc79d877bfef8c51986dc4903da9ccdfe5bfa
mithunkalan <mithunkalan@gmail.com>
2020-05-16 14:15:26 +0200

    sd

--- src/admin.js
+++ src/admin.js
@@ -1,75 +1,155 @@
 import React from "react";
 import { graphqlOperation, API } from "aws-amplify";
-
-// import API, { graphqlOperation } from "@aws-amplify/api";
 import Auth from "@aws-amplify/auth";
-// import * as queries from "./graphql/queries";
 import * as mutations from "./graphql/mutations";
 import Amplify from "aws-amplify";
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import awsconfig from "./aws-exports";
 import Storage from "@aws-amplify/storage";
-
 Amplify.configure(awsconfig);
 Auth.configure(awsconfig);
 API.configure(awsconfig);
-// Storage.configure(awsconfig);
 class Admin extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      seek: 0,
+      movies: [],
 
-    this.state = {seek:0,
+      m:0,
 
-      movies: []
+      whatsplaying: "",
+      time: 0,
+      isOn: false,
+      start: 0,
+      dragalong:false,
+      dragalongtime:60
     };
-  }
+    this.startTimer = this.startTimer.bind(this);
+    this.stopTimer = this.stopTimer.bind(this);
+    this.resetTimer = this.resetTimer.bind(this);
 
+  }
   async getmovies() {
-    let s = await Storage.list('')
-    this.setState({ movies: s.filter(z=>z.key!=="") });
+    let s = await Storage.list("");
+    this.setState({ movies: s.filter((z) => z.key !== "") });
   }
-  
-async stop() {
-  await API.graphql(
-    graphqlOperation(mutations.createWatchwithToonsMessages, {   input: {
-        name: "asd",
-        command: "stop"
-      } })
-  );
-}
+  startTimer() {
+    this.setState({
+      isOn: true,
+      time: this.state.time,
+      start: Date.now() - this.state.time,
+    });
+    this.timer = setInterval(() => {
+      if ((this.state.time/1000).toFixed(0) % this.state.dragalongtime ===0 && this.state.dragalong) {
+        console.log(this.state.time)
+         API.graphql(
+          graphqlOperation(mutations.createWatchwithToonsMessages, {
+            input: {
+              name: (this.state.time/1000).toFixed(0),
+              command: "seek",
+            },
+          })
+        );
+      }
+      this.setState({
+        time: Date.now() - this.state.start,
+      });
+    }, 1000);
+  }
+  stopTimer() {
+    this.setState({ isOn: false });
+    clearInterval(this.timer);
+  }
+  resetTimer() {
+    this.setState({ time: 0, isOn: false });
+  }
+  async doThing(input, thing) {
+    if (thing === "play") {
+      this.setState({ whatsplaying: input });
+      this.startTimer();
+    }
+    if (thing === "start") {
+      this.startTimer();
+    }
+    if (thing === "load") {
+      this.setState({ whatsplaying: input });
 
-  async play(input) {
+      this.stopTimer();
+      this.resetTimer();
+    }
+    if (thing === "stop") {
+      this.setState({ whatsplaying: "" });
+      this.stopTimer();
+      this.resetTimer();
+    }
+    if (thing === "pause") {
+      // this.setState({ whatsplaying: "" });
+      this.stopTimer();
+      // this.resetTimer();
+    }
+    if (thing === "unpause") {
+      // this.setState({ whatsplaying: "" });
+      this.startTimer();
+      // this.resetTimer();
+    }
+
     await API.graphql(
-      graphqlOperation(mutations.createWatchwithToonsMessages, {   input: {
+      graphqlOperation(mutations.createWatchwithToonsMessages, {
+        input: {
           name: input,
-          command: "play"
-        } })
+          command: thing,
+        },
+      })
     );
   }
-  async seek(input) {
-    console.log(input)
-    await API.graphql(
-      graphqlOperation(mutations.createWatchwithToonsMessages, {   input: {
-          name: input,
-          command: "seek"
-        } })
-    );
+  componentDidMount() {
+    this.getmovies();
   }
+
   render() {
-    // console.log(this.state)
     return (
       <div style={{ display: "flex", flexDirection: "column" }}>
         <button onClick={() => Auth.signOut()}>signout</button>
         <button onClick={() => this.getmovies()}>getmovies</button>
         {this.state.movies.map((z, idx) => (
           <div key={idx}>
-            <button onClick={() => this.play(z.key)}>PLAY</button>
+            <button onClick={() => this.doThing(z.key, "play")}>PLAY</button>
+            <button onClick={() => {this.doThing(z.key, "load")}}>LOAD</button>
+            <button onClick={() => this.doThing(z.key, "start")}>START</button>
             {z.key}
           </div>
         ))}
-        <button onClick={() => this.stop()}>stop</button>
-      <input value={this.state.seek} onChange={change=>this.setState({seek:parseInt(change.target.value)})} ></input>
-      <button onClick={() => this.seek(this.state.seek)}>seek</button>
+        <button onClick={() => this.doThing("", "stop")}>stop</button>
+        <button onClick={() => this.doThing("", "pause")}>pause</button>
+        <button onClick={() => this.doThing("", "unpause")}>unpause</button>
+        <button onClick={() => this.doThing("", "checkbuffer")}>check buffer</button>
+        <button onClick={() => this.setState({dragalong:!this.state.dragalong})}>toggle drag along</button>
+        <button onClick={() => this.setState({dragalongtime:this.state.dragalong+30})}>drag along +30</button>
+        <button onClick={() => this.setState({dragalongtime:this.state.dragalong-30})}>drag along -30</button>
+        <input
+          value={this.state.seek}
+          onChange={(change) =>
+            this.setState({ seek: parseInt(change.target.value) })
+          }
+        ></input>
+        <input
+
+          value={this.state.seek/60}
+          onChange={(change) =>
+            this.setState({ seek: parseInt(change.target.value)*60 })
+          }
+        ></input>
+        <button onClick={() => this.doThing(this.state.seek, "seek")}>
+          seek
+        </button>
+        currently playing :{this.state.whatsplaying}
+        <br />
+        time:{(this.state.time / 1000).toFixed(0)}
+        <br/>
+        drag along: {this.state.dragalong?"true":"false"}
+        <br/>
+        drag along time: {this.state.dragalongtime}
       </div>
     );
   }
--- src/client.js
+++ src/client.js
@@ -1,57 +1,69 @@
 import React from "react";
 import ReactPlayer from "react-player";
 import { graphqlOperation, API } from "aws-amplify";
-
-// import API, { graphqlOperation } from "@aws-amplify/api";
 import Storage from "@aws-amplify/storage";
 import Auth from "@aws-amplify/auth";
-// import * as queries from "./graphql/queries";
 import Amplify from "aws-amplify";
 import awsconfig from "./aws-exports";
 import * as subscriptions from "./graphql/subscriptions";
 Amplify.configure(awsconfig);
 Auth.configure(awsconfig);
 API.configure(awsconfig);
-// Storage.configure(awsconfig);
 class Client extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      nomovies: true
+      nomovies: true,
+      playing: true,
     };
-      this.ref = React.createRef();
+    this.ref = React.createRef();
   }
   async doplay(input) {
     if (input.value.data.onCreateWatchwithToonsMessages.command === "play") {
       let moviename = input.value.data.onCreateWatchwithToonsMessages.name;
       let file = await Storage.get(moviename, {
-        expires: 9999999
+        expires: 9999999,
       });
-      this.setState({filer:file,nomovies:false})
+      this.setState({ filer: file, nomovies: false, playing: true });
+    }
+    if (input.value.data.onCreateWatchwithToonsMessages.command === "load") {
+      let moviename = input.value.data.onCreateWatchwithToonsMessages.name;
+      let file = await Storage.get(moviename, {
+        expires: 9999999,
+      });
+      this.setState({ filer: file, nomovies: false, playing: false });
     }
     if (input.value.data.onCreateWatchwithToonsMessages.command === "stop") {
-
-      this.setState({filer:null,nomovies:true})
+      this.setState({ filer: null, nomovies: true });
+    }
+    if (input.value.data.onCreateWatchwithToonsMessages.command === "start") {
+      this.setState({ playing: true });
     }
     if (input.value.data.onCreateWatchwithToonsMessages.command === "seek") {
-
-      this.ref.current.seekTo(input.value.data.onCreateWatchwithToonsMessages.name, "seconds");
+      this.ref.current.seekTo(
+        input.value.data.onCreateWatchwithToonsMessages.name,
+        "seconds"
+      );
+    }
+    if (input.value.data.onCreateWatchwithToonsMessages.command === "pause") {
+      this.setState({ playing: false });
+    }
+    if (input.value.data.onCreateWatchwithToonsMessages.command === "checkbuffer") {
+      console.log(this.ref)
+      console.log(this.ref.current.getSecondsLoaded())
+    }
+    if (input.value.data.onCreateWatchwithToonsMessages.command === "unpause") {
+      this.setState({ playing: true});
     }
   }
 
-async getlist(){
-
-  // let s = await Storage.list('public/')
-  // console.log(s)
-}
   componentDidMount() {
-     API.graphql(
+    API.graphql(
       graphqlOperation(subscriptions.onCreateWatchwithToonsMessages)
     ).subscribe({
-      next: todoData => this.doplay(todoData)
+      next: (todoData) => this.doplay(todoData),
     });
-      this.getlist()
   }
 
   render() {
@@ -64,7 +76,7 @@ async getlist(){
             height: "100vh",
             display: "flex",
             alignItems: "center",
-            justifyContent: "center"
+            justifyContent: "center",
           }}
         >
           No Movies :(
@@ -74,7 +86,7 @@ async getlist(){
       return (
         <div style={{ backgroundColor: "#000", color: "#666" }}>
           <ReactPlayer
-            playing
+            playing={this.state.playing}
             url={this.state.filer}
             ref={this.ref}
             controls
